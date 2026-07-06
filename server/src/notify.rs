1a0a95bc3ca8464912094428926ae31cdb4a5e52
xRealBlue <heetvavadiya456@gmail.com>
2026-07-07 00:02:15 +0530

    add broadcast notification channel

--- server/src/notify.rs
+++ server/src/notify.rs
@@ -1,1 +1,33 @@
-pub fn setup_notification_channel() {}
+use crate::models::{Email, EmailEvent};
+use tokio::sync::broadcast;
+
+const CHANNEL_CAPACITY: usize = 128;
+
+pub type NotificationSender = broadcast::Sender<EmailEvent>;
+pub type NotificationReceiver = broadcast::Receiver<EmailEvent>;
+
+pub fn setup_notification_channel() -> NotificationSender {
+    let (tx, _rx) = broadcast::channel::<EmailEvent>(CHANNEL_CAPACITY);
+    tracing::info!(
+        "Notification channel created (capacity: {})",
+        CHANNEL_CAPACITY
+    );
+    tx
+}
+
+pub fn send_notifications_channel(tx: &NotificationSender, event: &EmailEvent) -> bool {
+    match tx.send(event.clone()) {
+        Ok(receiver_count) => {
+            tracing::debug!("Notification sent to {} receiver(s)", receiver_count);
+            receiver_count > 0
+        }
+        Err(broadcast::error::SendError(_)) => {
+            tracing::debug!("No active SSE receivers");
+            false
+        }
+    }
+}
+
+pub fn subscribe(tx: &NotificationSender) -> NotificationReceiver {
+    tx.subscribe()
+}
